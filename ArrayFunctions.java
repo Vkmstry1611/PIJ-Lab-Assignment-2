da25dc556e2fa63fca6c9b62a80e0a9ad4123525
Vkmstry1611 <89677101+Vkmstry1611@users.noreply.github.com>
2025-01-29 11:45:39 +0530

    added comments

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -2,49 +2,32 @@ import java.util.*;
 
 class ArrayFunctions{
 
-
+    // Function to print the array
     public void display(ArrayList<Integer> array){
 
             System.out.println("Array is : "+array);
 
-
-
         }
 
 
-
+    // Fucntion to add elements to odd even array
     public void oddEven(int[] array){
 		
-		
+		// declare array
 		ArrayList<Integer> even = new ArrayList<Integer>();
 		ArrayList<Integer> odd = new ArrayList<Integer>();	
-	
 
 
-		//int[] odd = new int[5];
-		//int[] even = new int [5];
-	
-	
-		//int ODD_INDEX=0;
-		//int EVEN_INDEX=0;
-	
-
-
+        //seperate odd and even elements
 		for (int i = 0; i < array.length; i++){
 		
 
 			if (array[i] % 2 == 0 ){
-
-				//even[EVEN_INDEX] = array[i];
-				//EVEN_INDEX++;
 				
 				even.add(array[i]);
 
 			}
 			else{
-
-				//odd[ODD_INDEX] = array[i];
-				//ODD_INDEX++;
 
 				odd.add(array[i]);
 
