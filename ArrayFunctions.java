e5586e0b3cf975c8ae9ab0d15d4548c8216adf79
Vkmstry1611 <89677101+Vkmstry1611@users.noreply.github.com>
2025-01-29 11:49:24 +0530

    added consecutive difference function

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -44,6 +44,25 @@ class ArrayFunctions{
 	}
 
 
+    // function to calculate consecutive elements difference
+    public int minConsecutiveDifference(int[] array){
+
+
+		int index = 0;
+		for (int i = 0; i < array.length-1 ;i++ ){
+		
+
+			if(array[i]-array[i+1] < array[index]-array[index+1]){
+			
+				index=i;
+			}
+
+		}
+
+    //return the first index 
+	return index;
+
+	}
 
         
 }
