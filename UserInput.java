f65c487ebbc80df254191949a42ce71cd387eaa6
Vkmstry1611 <89677101+Vkmstry1611@users.noreply.github.com>
2025-01-29 11:52:17 +0530

    added array input function

--- UserInput.java
+++ UserInput.java
@@ -1,7 +1,24 @@
+import java.util.*;
+
 class UserInput{
 
 
+    // function to take user input for array of 5 elements
+    int[] arrayInput(){
+
+		Scanner scan = new Scanner(System.in);
+		int[] array = new int[5];
+		System.out.println("Enter 5 Numbers : ");
+
+		for(int i=0 ; i < array.length ; i++){
+		
+			array[i] = scan.nextInt();
+
+		}
+		
+		return array;
+
+	}
 
 
-     
 }
