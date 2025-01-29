89fcebb60ba874e598cfbc72ba2d2b8a788f6f5e
Vkmstry1611 <89677101+Vkmstry1611@users.noreply.github.com>
2025-01-29 11:42:42 +0530

    added oddEven Function

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -12,5 +12,55 @@ class ArrayFunctions{
         }
 
 
+
+    public void oddEven(int[] array){
+		
+		
+		ArrayList<Integer> even = new ArrayList<Integer>();
+		ArrayList<Integer> odd = new ArrayList<Integer>();	
+	
+
+
+		//int[] odd = new int[5];
+		//int[] even = new int [5];
+	
+	
+		//int ODD_INDEX=0;
+		//int EVEN_INDEX=0;
+	
+
+
+		for (int i = 0; i < array.length; i++){
+		
+
+			if (array[i] % 2 == 0 ){
+
+				//even[EVEN_INDEX] = array[i];
+				//EVEN_INDEX++;
+				
+				even.add(array[i]);
+
+			}
+			else{
+
+				//odd[ODD_INDEX] = array[i];
+				//ODD_INDEX++;
+
+				odd.add(array[i]);
+
+
+			}
+
+		
+
+		
+		}
+
+	display(odd);
+	display(even);
+	}
+
+
+
         
 }
