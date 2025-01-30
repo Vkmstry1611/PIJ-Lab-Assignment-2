494808034cad6fbc31de38d589f3ea8527e7bce0
Vkmstry1611 <89677101+Vkmstry1611@users.noreply.github.com>
2025-01-30 09:44:29 +0530

    edited main

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 // Name : Vidish Ketankumar Mistry
 // PRN : 23070126146
 // Batch : B-3 
@@ -12,15 +15,24 @@ class Main{
 
         int[] arr= in.arrayInput();
 
+        ArrayList<Integer> array_list= new ArrayList<Integer>();
         
 		functions.display(functions.arrayToList(arr));
 
 		functions.oddEven(arr);
 		
 
-
 		int index =  functions.minConsecutiveDifference(arr);
 		System.out.println("Min Difference Index is : "+index);
+
+        array_list=functions.arrayToList(arr);
+        System.out.println("Array Converted to ArrayList : " +array_list);
+
+
+        arr=functions.listToArray(array_list);
+        System.out.println("ArrayList converted to Array : ");
+;       functions.display(arr);
+
         
 
 
